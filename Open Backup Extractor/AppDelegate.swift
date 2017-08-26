b078c63967d6c3391e29cb6b226618e0316587b5
vgmoose <me@vgmoose.com>
2017-08-26 16:53:11 -0400

    Initial Commit

---
+++ Open Backup Extractor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Open Backup Extractor
+//
+//  Created by vgm on 8/26/17.
+//  Copyright © 2017 VGMoose. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Open Backup Extractor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Open Backup Extractor
+//
+//  Created by vgm on 8/26/17.
+//  Copyright © 2017 VGMoose. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
