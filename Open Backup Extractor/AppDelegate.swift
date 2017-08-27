8cd19e3ca199e287665a5a8c177c5ea24a9eef6c
vgmoose <me@vgmoose.com>
2017-08-26 21:53:16 -0400

    refactor to to support iOS 10.9

--- Open Backup Extractor/AppDelegate.swift
+++ Open Backup Extractor/AppDelegate.swift
@@ -13,8 +13,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
 
-	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
+	func applicationDidFinishLaunching(_ aNotification: Notification)
+	{
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
--- Open Backup Extractor/ViewController.swift
+++ Open Backup Extractor/ViewController.swift
@@ -10,12 +10,6 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-
-		// Do any additional setup after loading the view.
-	}
-
 	override var representedObject: Any? {
 		didSet {
 		// Update the view, if already loaded.
